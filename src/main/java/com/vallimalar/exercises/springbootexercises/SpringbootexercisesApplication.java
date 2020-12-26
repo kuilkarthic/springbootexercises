a28cd60398ea6b4062610dfd1e1dbc40ae75f613
Vallimalar Karthick <kuilkarthic@gmail.com>
2020-12-26 23:24:56 +0000

    First Commit

---
+++ src/main/java/com/vallimalar/exercises/springbootexercises/SpringbootexercisesApplication.java
@@ -0,0 +1,13 @@
+package com.vallimalar.exercises.springbootexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootexercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vallimalar/exercises/springbootexercises/SpringbootexercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vallimalar.exercises.springbootexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootexercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
